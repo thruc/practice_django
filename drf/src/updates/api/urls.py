6a7230619e03992338eaae87e2d56918fba7974a
thruc <horiuchi.github@gmail.com>
2021-06-12 18:59:11 +0900

    サンプル

--- drf/src/updates/api/urls.py
+++ drf/src/updates/api/urls.py
@@ -2,10 +2,18 @@ from django.conf.urls import url
 
 from .views import (
             UpdateModelDetailAPIView,
-            UpdateModelListAPIView 
+            UpdateModelListAPIView,
     )
+
+from ..views import (
+    JsonCBV2,
+    JsonCBV
+)
 
 urlpatterns = [
     url(r'^$', UpdateModelListAPIView.as_view()), # api/updates/ - List/Create
     url(r'^(?P<id>\d+)/$', UpdateModelDetailAPIView.as_view()),
+    url(r'^vba/$', JsonCBV2.as_view()),
+    url(r'^vba2/$', JsonCBV.as_view()),
+
 ]
